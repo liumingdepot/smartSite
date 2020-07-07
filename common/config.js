20b57cf7921ff3a9a24c0671112839d14a7c0481
liuming <962398309@qq.com>
2020-07-07 14:32:59 +0800

    健康页面接口完成，

--- common/config.js
+++ common/config.js
@@ -1,1 +1,2 @@
-export const apiUrl = 'http://192.168.2.123:8001/'
+export const apiUrl = 'http://113.140.66.229:11016/'//线上
+// export const apiUrl = 'http://192.168.2.123:8001/'//测试
--- pages/health/health.vue
+++ pages/health/health.vue
@@ -1,18 +1,63 @@
 <template>
 	<view>
-		健康填报
+		<health-none v-if="show == 1" @changeShow="changeShow" />
+		<health-add v-if="show == 2" :healJson="healJson" @changeShow="changeShow" @appSave="appSave"/>
+		<health v-if="show == 3" :healJson="healJson" @changeShow="changeShow" />
 	</view>
 </template>
 
 <script>
+	import healthAdd from '@/components/health/health-add.vue'
+	import healthNone from '@/components/health/health-none.vue'
+	import health from '@/components/health/health.vue'
+	import {getHealth,updateHeal} from './server.js'
 	export default {
+		components:{
+			healthAdd,
+			healthNone,
+			health
+		},
 		data() {
 			return {
-				
+				show:0,
+				healJson:{}
 			};
+		},
+		onLoad() {
+			this.initData()
+		},
+		methods:{
+			async initData(){
+				this.healJson = await getHealth()
+				if(this.healJson.healthid){
+					this.show = 3
+				}else{
+					this.show = 1
+				}
+			},
+			appSave(healJson){
+				const ob = {
+					token:uni.getStorageSync('token'),
+					userid:uni.getStorageSync('userid'),
+					workid:this.healJson.workid,
+					healJson
+				}
+				if(this.healJson.healthid){
+					ob.healJson.healthid = this.healJson.healthid
+				}
+				updateHeal(ob).then(res=>{
+					if(res.code == 1){
+						this.initData()
+					}
+				})
+			},
+			changeShow(show){
+				this.show = show
+			}
 		}
 	}
 </script>
+
 
 <style lang="scss">
 
